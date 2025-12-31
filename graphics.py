d9104e4d53473921c4f4508323551404a62ed5c6
Zakariya Hashmi <zh60@rice.edu>
2025-12-31 13:22:58 -0600

    added comment to indicate that I will NOT be displaying on matplotlib, I will use the terminal

--- graphics.py
+++ graphics.py
@@ -3,37 +3,21 @@ import matplotlib.pyplot as plt
 from matplotlib.patches import Rectangle
 
 '''
-CREATE PLOTTING ENVIRONMENT
-import matplotlib.pyplot as plt
-fig, ax = plt.subplots()
-plt.show()
-
-CHANGE COLOR OF PLOTTING ENVIRONMENT
-ax.set_facecolor('blue')      # hex code works too
-
-PLOT LINE THROUGH TWO POINTS
-ax.plot([x1, x2], [y1, y2])
-
-CHANGE COLOR OF LINE
-ax.plot([x1, x2], [y1, y2], color='green')  # or 'c=' works too
-# or after creation:
-line.set_color('green')
-
-PLOT RECTANGLE AT A POINT
-from matplotlib.patches import Rectangle
-rect = Rectangle((x, y), width, height)  # (x, y) is bottom-left corner
-ax.add_patch(rect)
-
-CHANGE COLOR OF RECTANGLE
-rect = Rectangle((x, y), width, height, facecolor='red', edgecolor='black')
-# or after creation:
-rect.set_facecolor('red')
-rect.set_edgecolor('black')
+SCRAP IT, we will use the terminal instead
 '''
 
 '''
 given a point in 3D space, the projection of that point on the screen is
 (x', y') = (x/z, y/z)
+'''
+
+'''
+Order of points
+@@@@@@ - 1
+###### - 2
+====== - 3
+|||||| - 4
+...... - 5
 '''
 
 '''
